bf881608d7a9fc11d37234660e202cfb6ef67d78
Pierre-Yves Kerbrat <pkerbrat@free.fr>
2017-05-02 22:43:52 +0200

    sw: inside: read and display co2 & light levels
    Signed-off-by: Pierre-Yves Kerbrat <pkerbrat@free.fr>

--- sw/inside.cpp
+++ sw/inside.cpp
@@ -33,6 +33,7 @@ setup:
 #define TEMP_PIN 13
 #define LED_PIN 12
 #define LIGHT_SENSOR A0
+#define CO2_SENSOR A1
 
 int led_state = LOW;
 
@@ -58,34 +59,66 @@ void setup()
 	/* Configure VLD_BTN pin as input */
 	pinMode(VLD_BTN, INPUT);
 
+	/* Configure LIGHT & CO2 sensor pins as input */
+	pinMode(CO2_SENSOR, INPUT);
+	pinMode(LIGHT_SENSOR, INPUT);
+
 	// set up the LCD's number of columns and rows:
 	lcd.begin();
 	lcd.backlight();
 
 	lcd.clear();
 	lcd.setCursor(0,0);
-	lcd.print("INDISE");
+	lcd.print("INSIDE");
 	lcd.setCursor(0,1);
 	lcd.print("peewhy.net");
 	delay(2000);
 
 }
 
+static void update_screen(LiquidCrystal_I2C lcd, float temp, float hum,
+			  int light, int co2lvl)
+{
+	lcd.clear();
+	lcd.setCursor(0,0);
+	lcd.print("t:");
+	lcd.print(temp);
+	lcd.print("C  ");
+	lcd.print(" l:");
+	lcd.print(light);
+	lcd.setCursor(0,1);
+	lcd.print("h:");
+	lcd.print(hum);
+	lcd.print("%");
+	lcd.print(" CO2:");
+	lcd.print(co2lvl);
+	lcd.print(" ppm");
+}
 
 void loop()
 {
 	static unsigned long timer = millis();
 	static int deciSeconds = 0;
 	static int vldSts = 0;
+	static float co2lvl = 0;
+	static float lightlvl = 0;
 
 	if (millis() >= timer) {
 		deciSeconds++; /* 1000 ms is equal to 10 deciSecond */
 		timer += 1000;
 		if (deciSeconds == 10000) /* Reset to 0 after 1000 seconds. */
 			deciSeconds = 0;
+		/* Get sensor values */
+		co2lvl = analogRead(CO2_SENSOR);
+		lightlvl = analogRead(LIGHT_SENSOR);
+
 		vldSts = get_validButton();
 		Serial.print("Valid = ");
 		Serial.println(vldSts);
+		Serial.print("light = ");
+		Serial.print(lightlvl);
+		Serial.print(" CO2 = ");
+		Serial.println(co2lvl);
 
 		rtc_digitalClockDisplay();
 		if (led_state == HIGH)
@@ -93,6 +126,8 @@ void loop()
 		else
 			led_state = HIGH;
 		digitalWrite(LED_PIN, led_state);
+		update_screen(lcd, DHT.temperature, DHT.humidity, lightlvl,
+			      co2lvl);
 	}
 }
 
