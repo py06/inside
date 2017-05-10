e30d79b836338c11aee43d146b60d3bdf4606d6a
Pierre-Yves Kerbrat <pkerbrat@free.fr>
2017-05-10 10:26:59 +0200

    sw: keypad: use OneButton lib to manage keypad
    This lib helps detecting long press and other type of events
    on an input pin
    
    Signed-off-by: Pierre-Yves Kerbrat <pkerbrat@free.fr>

--- sw/keypad.cpp
+++ sw/keypad.cpp
@@ -1,7 +1,49 @@
 #include <Arduino.h>
+#include "OneButton.h"
 #include "keypad.h"
+
+int context;
+OneButton valid(VLD_BTN, true);
+
+int get_displayContext()
+{
+	return context;
+}
 
 int get_validButton()
 {
 	return digitalRead(VLD_BTN);
+}
+
+int get_upButton()
+{
+	return digitalRead(UP_BTN);
+}
+int get_downButton()
+{
+	return digitalRead(DWN_BTN);
+}
+
+int keypad_status()
+{
+	return get_validButton()<<2 | get_upButton()<<1 | get_downButton();
+}
+
+void manage_keypad(){
+	valid.tick();
+}
+
+static void context_switch() {
+	context = !context;
+}
+
+void init_keypad()
+{
+	context = DISPLAY_STANDARD;
+	valid.setPressTicks(LONG_PRESS_TIME);
+	valid.attachLongPressStart(context_switch);
+}
+
+void deinit_keypad()
+{
 }
--- sw/keypad.h
+++ sw/keypad.h
@@ -1,3 +1,22 @@
-#define VLD_BTN 11
+#define VLD_BTN 2
+#define VLD_INT 0
 
+#define UP_BTN 10
+#define DWN_BTN 9
+
+/* timings */
+#define DEBOUNCE_TIME 250 /* 250ms debouncing perdiod */
+#define LONG_PRESS_TIME (4 * 1000) /* 4s to siwtch in setting mode */
+
+/* display context */
+#define DISPLAY_STANDARD 0 /* normal display mode */
+#define DISPLAY_SETTINGS 1 /* in settings and config context */
+
+extern int context;
+
+int get_displayContext(void);
 int get_validButton(void);
+int keypad_status(void);
+void init_keypad(void);
+void deinit_keypad(void);
+void manage_keypad (void);
