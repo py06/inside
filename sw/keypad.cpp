559a9906dc8f5889402039ef99daa9bdee538557
Pierre-Yves Kerbrat <pkerbrat@free.fr>
2017-05-12 20:23:38 +0200

    sw: keypad: generate events for all keys
    Signed-off-by: Pierre-Yves Kerbrat <pkerbrat@free.fr>

--- sw/keypad.cpp
+++ sw/keypad.cpp
@@ -2,8 +2,12 @@
 #include "OneButton.h"
 #include "keypad.h"
 
-int context;
+int context = DISPLAY_STANDARD;
+int events = 0;
+
 OneButton valid(VLD_BTN, true);
+OneButton up(UP_BTN, true);
+OneButton down(DWN_BTN, true);
 
 int get_displayContext()
 {
@@ -31,17 +35,35 @@ int keypad_status()
 
 void manage_keypad(){
 	valid.tick();
+	up.tick();
+	down.tick();
 }
 
-static void context_switch() {
-	context = !context;
+static void validLongPress() {
+	events |= VALID_LONG_PRESS;
+}
+
+static void validPress() {
+	events |= VALID_PRESS;
+}
+
+static void upPress() {
+	events |= UP_PRESS;
+}
+
+static void downPress() {
+	events |= DOWN_PRESS;
 }
 
 void init_keypad()
 {
 	context = DISPLAY_STANDARD;
+	events = 0;
 	valid.setPressTicks(LONG_PRESS_TIME);
-	valid.attachLongPressStart(context_switch);
+	valid.attachLongPressStart(validLongPress);
+	valid.attachClick(validPress);
+	up.attachClick(upPress);
+	down.attachClick(downPress);
 }
 
 void deinit_keypad()
--- sw/keypad.h
+++ sw/keypad.h
@@ -8,11 +8,18 @@
 #define DEBOUNCE_TIME 250 /* 250ms debouncing perdiod */
 #define LONG_PRESS_TIME (4 * 1000) /* 4s to siwtch in setting mode */
 
+/* events */
+#define VALID_PRESS (1 << 0)
+#define VALID_LONG_PRESS (1 << 1)
+#define UP_PRESS (1 << 2)
+#define DOWN_PRESS (1 << 3)
+
 /* display context */
 #define DISPLAY_STANDARD 0 /* normal display mode */
 #define DISPLAY_SETTINGS 1 /* in settings and config context */
 
 extern int context;
+extern int events;
 
 int get_displayContext(void);
 int get_validButton(void);
